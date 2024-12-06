14cd9c6f7415afa29dd58154af6fc3f723dd22e3
mahfuzur15 <mahfuzur15-10236@diu.edu.bd>
2024-12-06 09:31:47 +0600

    New Changes with jenkins setup

---
+++ test.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Get Source Code') {
+            steps {
+                git credentialsId: 'a5499089-a6f6-46bb-890f-c98802efbca2', url: 'https://github.com/Mahfuzur15/Jenkinscheck.git'
+                echo 'Hello World'
+            }
+        }
+        stage('Build Code')
+        {
+            steps
+            {
+            bat script: 'mvn compile'
+            }
+        }
+         stage('Run Test')
+        {
+            steps
+            {
+            bat script: 'mvn test -Dbrowser=localchrome'
+            }
+        }
+        stage('Publish Report')
+        {
+            steps{
+            publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '', reportFiles: 'target/surefire-reports/Extent*.html', reportName: 'HTML Report Pipeline', reportTitles: '', useWrapperFileDirectly: true])
+            }
+        }
+    }
+}
