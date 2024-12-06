340977d4e49ef41ca29245a60607291f50661e5b
mahfuzur15 <mahfuzur15-10236@diu.edu.bd>
2024-12-06 11:30:02 +0600

    New Changes with jenkins setup

--- test.groovy
+++ test.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Get Source Code') {
             steps {
-                git credentialsId: 'a5499089-a6f6-46bb-890f-c98802efbca2', url: 'https://github.com/Mahfuzur15/Jenkinscheck.git'
+                git credentialsId: 'a5499089-a6f6-46bb-890f-c98802efbca2', url: 'https://github.com/Mahfuzur15/Myalice-24-task.git'
                 echo 'Hello World'
             }
         }
